785271f977e508a87f0117ca24736d48e21c0b25
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-06 23:34:56 +0700

    Add gameLost

--- Scripts/GameStateManager.cs
+++ Scripts/GameStateManager.cs
@@ -1,14 +1,24 @@
+using System.Data;
 using Godot;
 
 public partial class GameStateManager : Node
 {
+	public event MinesGrid.GameLostEventHandler gameLost;
+	Timer timer = new Timer();
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		gameLost += OnGameLost;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void OnGameLost()
+	{
+		timer.Stop();
 	}
 }
