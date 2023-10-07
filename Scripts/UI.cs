bf9eb1815335fb84c56ac461986a9c04afb17fb0
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-07 11:59:12 +0700

    Add GetNode()

--- Scripts/UI.cs
+++ Scripts/UI.cs
@@ -1,11 +1,12 @@
 using Godot;
-using System;
 
 public partial class UI : CanvasLayer
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("yang ini");
+		GD.Print(GetNode("MinesCountLabel"));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
