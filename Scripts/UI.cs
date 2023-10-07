07e24af3bbce2d3e1c122d6af972be2b63f2d9c1
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-07 12:02:40 +0700

    Add minesCountLabel

--- Scripts/UI.cs
+++ Scripts/UI.cs
@@ -2,11 +2,12 @@ using Godot;
 
 public partial class UI : CanvasLayer
 {
+	Label minesCountLabel;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("yang ini");
-		GD.Print(GetNode("MinesCountLabel"));
+		minesCountLabel = GetNode<Label>("MinesCountLabel");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
