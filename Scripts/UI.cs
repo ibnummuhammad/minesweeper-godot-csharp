ab55a20a7f9dfe11cfd14b82d882537acbb6d9c2
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-07 12:05:25 +0700

    Add timerCountLabel and gameStatusButton

--- Scripts/UI.cs
+++ Scripts/UI.cs
@@ -3,11 +3,15 @@ using Godot;
 public partial class UI : CanvasLayer
 {
 	Label minesCountLabel;
+	Label timerCountLabel;
+	TextureButton gameStatusButton;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		minesCountLabel = GetNode<Label>("MinesCountLabel");
+		timerCountLabel = GetNode<Label>("TimerCountLabel");
+		gameStatusButton = GetNode<TextureButton>("GameStatusButton");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
